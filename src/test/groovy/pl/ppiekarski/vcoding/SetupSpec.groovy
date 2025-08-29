d5b6f1fb8ae9a658ec5a816568825b6a26823e7c
Piotr Piekarski <piotrek199507@gmail.com>
2025-08-29 16:19:29 +0200

    refactor tests

--- src/test/groovy/pl/ppiekarski/vcoding/SetupSpec.groovy
+++ src/test/groovy/pl/ppiekarski/vcoding/SetupSpec.groovy
@@ -4,64 +4,37 @@ import spock.lang.Specification
 
 class SetupSpec extends Specification {
 
-    def "Should work example 1"() {
-        given:
-        int[] input = [4,3,2,7,8,2,3,1].toArray()
+    def "Should find numbers"() {
+        expect:
+        new Main().solution(input as int[]) == expected
 
-        when:
-        def result = new Main().solution(input)
-        then:
-        result == [5,6]
-    }
-
-    def "Should work example 2"() {
-        given:
-        int[] input = [1,1].toArray()
-
-        when:
-        def result = new Main().solution(input)
-        then:
-        result == [2]
+        where:
+        input                    || expected
+        [4, 3, 2, 7, 8, 2, 3, 1] || [5, 6]
+        [1, 1]                   || [2]
     }
 
 
-    def "Should work example s1"() {
-        given:
-        String input = "()[]{}"
+    def "Should strings"() {
+        expect:
+        new Main().isValid(input) == expected
 
-        when:
-        def result = new Main().isValid(input)
-        then:
-        result
-    }
+        where:
+        input      || expected
+        "()"       || true
+        "()[]{}"   || true
+        "([])"     || true
+        "{[()]}"   || true
+        "(((())))" || true
+        "{}([])"   || true
 
-    def "Should work example s2"() {
-        given:
-        String input = "(([{}]))"
-
-        when:
-        def result = new Main().isValid(input)
-        then:
-        result
-    }
-    def "Should work example s3"() {
-        given:
-        String input = "([{]})"
-
-        when:
-        def result = new Main().isValid(input)
-        then:
-        !result
-    }
-
-    def "Should work example s4"() {
-        given:
-        String input = "([{})"
-
-        when:
-        def result = new Main().isValid(input)
-        then:
-        !result
+        "("        || false
+        ")"        || false
+        "(]"       || false
+        "([)]"     || false
+        "((())"    || false
+        "(()))"    || false
+        "{[(])}"   || false
     }
 
 
